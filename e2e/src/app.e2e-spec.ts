c34e89d6250587c2bd440e850760abc5eddec6ed
julienboulay <jboulay@ekito.fr>
2019-03-28 09:39:44 +0100

    Add e2e test script

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -12,14 +12,11 @@ test('display 3 tabs', async t => {
   await t
     .expect(Selector('ion-tab-bar').exists).ok()
     .expect(Selector('ion-tab-bar ion-tab-button').count).eql(3)
-    
     .expect(firstTab.hasClass('tab-selected')).ok()
     .expect(firstTab.find('ion-icon').getAttribute('name')).eql('flash')
     .expect(firstTab.find('ion-label').innerText).eql('Tab One')
-  
     .expect(secondTab.find('ion-icon').getAttribute('name')).eql('apps')
     .expect(secondTab.find('ion-label').innerText).eql('Tab Two')
-  
     .expect(thirdTab.find('ion-icon').getAttribute('name')).eql('send')
     .expect(thirdTab.find('ion-label').innerText).eql('Tab Three');
 });
